f622260d9139e69960f234844aeae9121af087c6
yespickmeup <rpascua.synsoftech@gmail.com>
2018-06-27 21:12:47 +0800

    debug photo upload2

--- app/Http/Controllers/TeamMemberController.php
+++ app/Http/Controllers/TeamMemberController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\TeamMember;
 use App\Http\Controllers\UploadController;
+use App\Http\Requests\ImageUploadRequest;
 
 class TeamMemberController extends Controller
 {
