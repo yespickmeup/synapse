bf87dcab205d3e623d440794dd0f9c250462305f
yespickmeup <rpascua.synsoftech@gmail.com>
2018-06-28 11:39:35 +0800

    removed imageuploadrequest

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -9,39 +9,9 @@ use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
-    public function uploadFile($file,$path){
-        $imagename = $file->getClientOriginalName();
-        $imagename_woe = substr($imagename, 0 , (strrpos($imagename, ".")));
-        $extension = $file->getClientOriginalExtension();
-        $img_path = $path.$imagename;
-
-        $new_imgname = '';
-        if(!Storage::exists($img_path)){
-            $exists= 'No';
-            $imagename = $file->getClientOriginalName();
-            $file->storeAs($path,$imagename);
-            return $path.$imagename;
-        }else{
-            $loop = 0;
-            $i = 1;
-            do{
-                $new_imgname = $imagename_woe.'_'.$i.'.'.$extension;
-                $img_path = $path.$new_imgname;
-                if(!Storage::exists($img_path)){
-                    $file->storeAs($path,$new_imgname);
-                    return $path.$new_imgname;
-                    $loop =1; 
-                }
-                $i++;
-            }while($loop === 0);
-        }
-
-    }
+  
     
-    public function upload(ImageUploadRequest $request){
-
-
-
+    public function upload(Request $request){
         if($request->hasFile('image')){
             $imagename = $request->image->getClientOriginalName();
             $request->image->storeAs('public/images/banners',$imagename);
@@ -50,13 +20,10 @@ class UploadController extends Controller
             $banner->banner_img = '/images/banners/'.$imagename;
             if($banner->save()){
                 return $banner;
+            }else{
+                return "failed to update";
             }
-           
-            
         }
-        
-       
-
         return response()->json(['message:'=>'failed to upload','image'=>$request->image]);
     }
 }
